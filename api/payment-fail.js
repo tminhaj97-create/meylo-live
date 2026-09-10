e8c8cda55c9e9604001f4b01b34d2058a8a7b86d
Minhaz <tminhaj97@gmail.com>
2026-09-10 07:13:28 +0600

    Add payment-fail.js

---
+++ api/payment-fail.js
@@ -0,0 +1,20 @@
+// /api/payment-fail.js
+const admin = require('firebase-admin');
+if (!admin.apps.length) {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: `https://${serviceAccount.project_id}-default-rtdb.firebaseio.com`
+    });
+}
+
+module.exports = async (req, res) => {
+    const tranId = (req.body || {}).tran_id;
+    if (tranId) {
+        try {
+            await admin.database().ref(`orders/${tranId}/status`).set('failed');
+        } catch (err) { /* non-fatal */ }
+    }
+    res.writeHead(302, { Location: '/?payment=failed' });
+    res.end();
+};
